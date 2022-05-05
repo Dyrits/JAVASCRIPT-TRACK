fec5b7ffe566647ff327d6d0adadac677733de95
Dyrits <dyrits@outlook.com>
2022-05-05 15:36:08 -0500

    Initialization.

---
+++ lasagna/lasagna.js
@@ -0,0 +1,6 @@
+const PREPARATION_MINUTES_PER_LAYER = 2;
+export const EXPECTED_MINUTES_IN_OVEN = 40;
+
+export const remainingMinutesInOven = (actualMinutesInOven) => EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+export const preparationTimeInMinutes = (numberOfLayers) => PREPARATION_MINUTES_PER_LAYER * numberOfLayers;
+export const totalTimeInMinutes = (numberOfLayers, actualMinutesInOven) => preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
