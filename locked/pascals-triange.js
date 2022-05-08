94aba8a642389abe9b9d86a6b36f45d390720b46
Dyrits <dyrits@outlook.com>
2022-05-08 12:37:19 -0500

    Added solution to locked exercise.

---
+++ locked/pascals-triange.js
@@ -0,0 +1,19 @@
+const results = {
+    0: [],
+    1: [[1]],
+    2: [[1], [1, 2, 1]]
+}
+
+function rows(size) {
+    if (results[size]) { return results[size] }
+    let result = rows(size - 1);
+    let last = result[size - 2];
+    let current = [];
+    for (let i = 0; i < size; i++) {
+        let left = last[i - 1] || 0;
+        let right = last[i] || 0;
+        current[i] = left + right;
+    }
+    results[size] = result.concat([current]); // Adding the current result to the results.
+    return [...result, current]
+}
