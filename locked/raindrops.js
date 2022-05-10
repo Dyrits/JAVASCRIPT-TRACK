7fe610423a11ada25c18f27cd091dd880834a0f8
Dyrits <dyrits@outlook.com>
2022-05-10 02:09:07 -0500

    rain renamed to drops

--- locked/raindrops.js
+++ locked/raindrops.js
@@ -1,3 +1,3 @@
 const convert = (number) => Object.entries({ 3: "Pling", 5: "Plang", 7: "Plong" })
-        .reduce((rain, [value, drop]) =>  rain + (number % value ? String() : drop), String())
+        .reduce((drops, [value, drop]) =>  drops + (number % value ? String() : drop), String())
         || String(number);
