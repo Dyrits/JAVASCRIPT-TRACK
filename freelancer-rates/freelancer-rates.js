cf29e7bf1eba1a7cf251f201ddf4acb378804f41
Dyrits <dyrits@outlook.com>
2022-05-22 20:16:26 -0500

    freelancer-rates added.

---
+++ freelancer-rates/freelancer-rates.js
@@ -0,0 +1,33 @@
+/**
+ * The day rate, given a rate per hour
+ *
+ * @param {number} ratePerHour
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rate per day
+ */
+const dayRate = (ratePerHour, discount = 0) => ratePerHour * 8 * (1 - discount);
+
+/**
+ * Calculates the number of days in a budget, rounded down
+ *
+ * @param {number} budget: the total budget
+ * @param {number} ratePerHour: the rate per hour
+ * @returns {number} the number of days
+ */
+const daysInBudget = (budget, ratePerHour) => Math.floor(budget / dayRate(ratePerHour));
+
+/**
+ * Calculates the discounted rate for large projects, rounded up
+ *
+ * @param {number} ratePerHour
+ * @param {number} numDays: number of days the project spans
+ * @param {number} discount: for example 20% written as 0.2
+ * @returns {number} the rounded up discounted rate
+ */
+const priceWithMonthlyDiscount = (ratePerHour, numDays, discount) => {
+    const daysWithoutDiscount = numDays % 22;
+    const daysWithDiscount = numDays - daysWithoutDiscount;
+    return Math.ceil(dayRate(ratePerHour, discount) * daysWithDiscount + dayRate(ratePerHour) * daysWithoutDiscount);
+}
+
+export { dayRate, daysInBudget, priceWithMonthlyDiscount };
